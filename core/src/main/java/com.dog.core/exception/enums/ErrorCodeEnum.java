f22b6a4d5a66f1321146a39b95ba677b8798b033
Kelvin Pereira Biserra <42652204+Kelvin-Pereira@users.noreply.github.com>
2023-12-04 17:36:23 -0300

    first commit

---
+++ core/src/main/java/com.dog.core/exception/enums/ErrorCodeEnum.java
@@ -0,0 +1,5 @@
+package com.dog.core.exception.enums;
+
+public enum ErrorCodeEnum {
+
+}
---
+++ infrastructure/src/test/java/com/dog/infrastructure/GastroDeRuaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dog.infrastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastroDeRuaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
